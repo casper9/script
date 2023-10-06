80dfbf947cfe96c31eda45f916926ef73e5e319f
casper9 <50382728+casper9@users.noreply.github.com>
2023-10-07 01:35:39 +0800

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -46,7 +46,7 @@ if [ ! -f "etc/cron.d/autocpu" ]; then
 cat> /etc/cron.d/autocpu << END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-*/4 * * * * root /usr/bin/autocpu
+*/3 * * * * root /usr/bin/autocpu
 END
 fi
 
