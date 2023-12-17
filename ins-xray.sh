4fa4baaf25d8964c9f2380f1d91baeaa08a12869
casper9 <50382728+casper9@users.noreply.github.com>
2023-12-17 12:40:09 +0800

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -18,14 +18,6 @@ date
 echo ""
 cd
 domain=$(cat /etc/xray/domain)
-if [[ -e /etc/xray/domain ]]; then
-domain=$(cat /etc/xray/domain)
-else
-domain="casper1.dev"
-fi
-if [[ -z /etc/xray/domain ]]; then
-domain="casper1.dev"
-fi
 sleep 0.5
 mkdir -p /etc/xray
 echo -e "[ ${green}INFO${NC} ] Checking... "
@@ -420,7 +412,7 @@ cat >/etc/nginx/conf.d/xray.conf <<EOF
              listen [::]:8080;
              listen 443 ssl http2 reuseport;
              listen [::]:443 http2 reuseport;
-             server_name *.${domain};
+             server_name *.casper1.dev;
              ssl_certificate /etc/xray/xray.crt;
              ssl_certificate_key /etc/xray/xray.key;
              ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
